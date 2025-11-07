1b697beb307c7502448cc659aff45e0ee50c5703
Boris De Vloed <code@devloed.com>
2025-11-07 15:11:32 +0100

    added FEATURE_MITIGATE_API_LIMIT_WITH_AND_QUERY

--- constants.py
+++ constants.py
@@ -1,22 +1,32 @@
 import os
+
 RESOURCE_BASE = "http://data.lblod.info/id/"
 SCRAPE_JOB_TYPE = "http://lblod.data.gift/ns/ScrapeJob"
 SCRAPE_CAMPAIGN_JOB_TYPE = "http://lblod.data.gift/ns/ScrapeCampaignJob"
 SCRAPE_GRAPH = "http://mu.semte.ch/graphs/jobs-graph"
-DEFAULT_GRAPH = os.environ['DEFAULT_GRAPH'] or 'http://mu.semte.ch/graphs/public'
-FILE_STATUSES =  {
-    'READY': 'http://lblod.data.gift/file-download-statuses/ready-to-be-cached',
-    'ONGOING': 'http://lblod.data.gift/file-download-statuses/ongoing',
-    'COLLECTED': 'http://lblod.data.gift/file-download-statuses/collected',
-    'FAILURE': 'http://lblod.data.gift/file-download-statuses/failure',
+DEFAULT_GRAPH = os.environ["DEFAULT_GRAPH"] or "http://mu.semte.ch/graphs/public"
+FILE_STATUSES = {
+    "READY": "http://lblod.data.gift/file-download-statuses/ready-to-be-cached",
+    "ONGOING": "http://lblod.data.gift/file-download-statuses/ongoing",
+    "COLLECTED": "http://lblod.data.gift/file-download-statuses/collected",
+    "FAILURE": "http://lblod.data.gift/file-download-statuses/failure",
 }
 TASK_STATUSES = {
-    'BUSY': 'http://redpencil.data.gift/id/concept/JobStatus/busy',
-    'SCHEDULED': 'http://redpencil.data.gift/id/concept/JobStatus/scheduled',
-    'SUCCESS': 'http://redpencil.data.gift/id/concept/JobStatus/success',
-    'FAILED': 'http://redpencil.data.gift/id/concept/JobStatus/failed'
+    "BUSY": "http://redpencil.data.gift/id/concept/JobStatus/busy",
+    "SCHEDULED": "http://redpencil.data.gift/id/concept/JobStatus/scheduled",
+    "SUCCESS": "http://redpencil.data.gift/id/concept/JobStatus/success",
+    "FAILED": "http://redpencil.data.gift/id/concept/JobStatus/failed",
 }
 
 OPERATIONS = {
-    'COLLECTING': 'http://lblod.data.gift/id/jobs/concept/TaskOperation/collecting'
+    "COLLECTING": "http://lblod.data.gift/id/jobs/concept/TaskOperation/collecting"
 }
+
+API_URL = os.environ["API_URL"]
+
+FEATURE_MITIGATE_API_LIMIT_WITH_AND_QUERY = (
+    os.environ.get("FEATURE_MITIGATE_API_LIMIT_WITH_AND_QUERY", "false").lower()
+    == "true"
+)
+
+VERENIGINGS_TYPES = ["VZER", "VZW", "IVZW", "PS", "SVON", "FV", "SUB", "NB"]
